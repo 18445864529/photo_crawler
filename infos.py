f276f33f02a81f1a19fa0c4ce9800da142b49f22
bowenz <qq787204126@gmail.com>
2021-10-27 23:05:59 +0800

    Fix title parsing.

--- infos.py
+++ infos.py
@@ -38,7 +38,6 @@ def get_album_titles(html, num_plus_model=False):
         end = string.find('</a></p>')
         titles.append(string[start + 7:end])
         num_pattern = re.compile(r'No.{0,3}\d{1,5}', flags=re.I)
-        print(string)
         try:
             number = re.search(num_pattern, string).group()
         except:
